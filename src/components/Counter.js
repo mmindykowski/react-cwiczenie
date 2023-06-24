f286111bac4f00603eb77f1e6240f81555544c97
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-24 20:18:13 +0200

    plik counter

---
+++ src/components/Counter.js
@@ -0,0 +1,10 @@
+const Counter = () => {
+    return (
+        <div className="counter">
+            <p>Counter: 0</p>
+            <div></div>
+        </div>
+    );
+}
+
+export default Counter;
