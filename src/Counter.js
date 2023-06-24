19ddb0e75cf6300ff071767730df30c9cf477838
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-24 20:29:42 +0200

    stylowanie

--- src/Counter.js
+++ src/Counter.js
@@ -4,8 +4,8 @@ const Counter = () => {
   return (
     <div className="counter">
       <p>Counter: 0</p>
-      <div className="buttons-panel">
-        <button></button>
+      <div className="buttonsPanel">
+        <button>Add</button>
       </div>
     </div>
   );
