9ba09ad7949fb8fdd03dbd5767d8310dd7101d1d
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-24 20:36:50 +0200

    losowanie liczby do 10

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,11 @@
 import "./Counter.css";
 
 const Counter = () => {
+  const counter = Math.floor(Math.random() * 10);
+
   return (
     <div className="counter">
-      <p>Counter: 0</p>
+      <p>Counter: {counter}</p>
       <div className="buttonsPanel">
         <button>Add</button>
       </div>
