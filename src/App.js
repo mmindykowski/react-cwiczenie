ba2720de4b5d25d7edc7e38aad8651562a01fa0e
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-24 19:37:35 +0200

    pierwszy etap

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Heading = () => {
+  return <h2>React counter App</h2>
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+  <Heading />
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,5 @@
+const Heading = () => {
+  return <h2>React counter App</h2>;
+};
+
+export default Heading;
