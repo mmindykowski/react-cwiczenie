52c34d55bf19022d134fb49947bb2155e65bd3a9
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-06-25 22:01:30 +0200

    useeffect cykl zycia

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 
 const Clock = (props) => {
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-    useEffect(()=> {
-        console.log('komp');
+  useEffect(() => {
+    console.log("komp");
 
-        const interval = setInterval(()=>{
-            setTime(new Date().toLocaleTimeString())
-        }, 1000)
-    });
+    const interval = setInterval(() => {
+      console.log("wykonuje intwerwa");
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => {
+      console.log("odmontowuje interwal i czyszcze");
+      clearInterval(interval);
+    };
+  }, []);
 
-    return <p>{time}{""}
-    <span className="clockControll" onClick={()=>props.setShowClock(false)}>x</span></p>
+  return (
+    <p>
+      {time}
+      {""}
+      <span className="clockControll" onClick={() => props.setShowClock(false)}>
+        x
+      </span>
+    </p>
+  );
 };
 
 export default Clock;
